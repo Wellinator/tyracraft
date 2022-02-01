7b8907d709fa08a04f0679394f6924abb1637d1e
wellinator <wellcoj@gmail.com>
2022-02-01 12:30:51 -0300

    feat: create world base

---
+++ objects/World.cpp
@@ -0,0 +1,22 @@
+
+#include "World.hpp"
+
+void World::init()
+{
+    //TODO:
+    //Generate terrain
+    //Create initial chunk by terrain position offset
+    return;
+};
+
+void World::update()
+{
+    //TODO:
+    //Update chunck through terrain while player change position
+    return;
+};
+
+void World::render(Engine *t_engine)
+{
+    return;
+};
