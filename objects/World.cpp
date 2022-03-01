4641922a4ff1ab77ae4bcd770aa6f091db115cdb
wellinator <wellcoj@gmail.com>
2022-03-01 16:08:35 -0300

    feat: update chunck by player position

--- objects/World.cpp
+++ objects/World.cpp
@@ -13,16 +13,19 @@ void World::init()
     PRINT_LOG("Creating World");
     this->terrainManager->init(engine);
     this->chunck = this->terrainManager->getChunck(0, 0, 0);
-    // TODO:
-    // Update get chunck by player position
 
     PRINT_LOG("World created!");
     return;
 };
 
-void World::update()
+void World::update(Player *t_player)
 {
+    // TODO: If player move, then update chunck by player position;
+    // TODO: If Player has moved -> rebuild the chunck;
+    this->terrainManager->updateChunkByPlayerPosition(t_player);
+    
     this->terrainManager->update();
     this->chunck->renderer();
-    return;
+    // TODO:
+    // Update get chunck by player position
 };
