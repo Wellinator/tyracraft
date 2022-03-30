a72a3e276dc750659cbd4a4369ca1fd749de9829
Wellinator <wellcoj@gmail.com>
2022-03-29 21:30:26 -0300

    feat: increase chunck size

--- include/contants.hpp
+++ include/contants.hpp
@@ -1,8 +1,8 @@
 #ifndef _CONSTANTS_
 #define _CONSTANTS_
 
-#define OVERWORLD_H_DISTANCE 256/2                           // Total horizontal overworld distance;
-#define OVERWORLD_V_DISTANCE 64/2                            // Total vertical overworld distance;
+#define OVERWORLD_H_DISTANCE 256 / 2                       // Total horizontal overworld distance;
+#define OVERWORLD_V_DISTANCE 64 / 2                        // Total vertical overworld distance;
 #define HALF_OVERWORLD_H_DISTANCE OVERWORLD_H_DISTANCE / 2 // Half horizontal overworld distance;
 #define HALF_OVERWORLD_V_DISTANCE OVERWORLD_V_DISTANCE / 2 // Half vertical overworld distance;
 #define OVERWORLD_MIN_DISTANCE -HALF_OVERWORLD_H_DISTANCE  // Min overworld width;
@@ -11,7 +11,7 @@
 #define OVERWORLD_MAX_HEIGH HALF_OVERWORLD_V_DISTANCE      // Max overworld heigh layer;
 
 #define OVERWORLD_SIZE OVERWORLD_H_DISTANCE *OVERWORLD_H_DISTANCE *OVERWORLD_V_DISTANCE
-#define CHUNCK_SIZE 12
+#define CHUNCK_SIZE 24
 #define HALF_CHUNCK_SIZE CHUNCK_SIZE / 2
 #define BLOCK_SIZE 7.0F
 
@@ -24,5 +24,5 @@
 
 #define FIRST_PERSON_CAM 1
 #define THIRD_PERSON_CAM 2
-    
+
 #endif
