0d403e52063b525dafab5a1158db06051272b00f
Wellinator <wellcoj@gmail.com>
2022-07-23 11:52:54 -0300

    refactor: BlockItem to BlockTexOffset

--- inc/managers/block_manager.hpp
+++ inc/managers/block_manager.hpp
@@ -4,23 +4,27 @@
 #include <tamtypes.h>
 #include <renderer/renderer.hpp>
 #include <renderer/3d/mesh/mesh.hpp>
+#include <renderer/core/texture/models/texture.hpp>
 #include "contants.hpp"
 
 using Tyra::Mesh;
 using Tyra::Renderer;
+using Tyra::Texture;
 
-struct BlockItem {
-  BlockItem(Mesh* mesh, u8 type) {
-    t_mesh = mesh;
+struct BlockTexOffset {
+  BlockTexOffset(u8 type, u8 isSingle, const float& texOffssetX,
+                 const float& texOffssetY) {
+    _texOffssetX = texOffssetX;
+    _texOffssetY = texOffssetY;
     blockId = type;
+    _isSingle = isSingle;
   };
 
-  ~BlockItem() {
-    delete t_mesh;
-    t_mesh = NULL;
-  };
+  ~BlockTexOffset(){};
 
-  Mesh* t_mesh;
+  float _texOffssetX;
+  float _texOffssetY;
+  u8 _isSingle;
   u8 blockId;
 };
 
@@ -29,55 +33,13 @@ class BlockManager {
   BlockManager();
   ~BlockManager();
   void init(Renderer* t_renderer);
-  void linkTextureByBlockType(u8 blockType, const u32 t_meshId,
-                              u8 materialIndex);
-  void removeTextureLinkByBlockType(u8 blockType, const u32 t_meshId,
-                                    u8 materialIndex);
-  Mesh* getMeshByBlockType(u8 blockType);
+  BlockTexOffset* getBlockTexOffsetByType(u8 blockType);
 
  private:
-  void pushItems();
-  void loadBlocks();
+  void registerBlocksTextureCoordinates();
+  void loadBlocksTextures();
 
+  Texture* blocksTexAtlas;
   Renderer* t_renderer;
-  std::vector<BlockItem*> blockItems;
-
-  /**
-   * -----------BLocks meshes-----------
-   */
-  // Blocks
-  //   Mesh stoneBlock;
-  //   Mesh grassBlock;
-  //   Mesh dirtBlock;
-  //   Mesh waterBlock;
-  //   Mesh bedrockBlock;
-  //   Mesh sandBlock;
-  //   Mesh glassBlock;
-  //   Mesh bricksBlock;
-
-  // Ores and Minerals
-  //   Mesh goldOreBlock;
-  //   Mesh ironOreBlock;
-  //   Mesh coalOreBlock;
-  //   Mesh diamondOreBlock;
-  //   Mesh redsoneOreBlock;
-  //   Mesh emeraldOreBlock;
-
-  // Wood Planks
-  //   Mesh oakPlanksBlock;
-  //   Mesh sprucePlanksBlock;
-  //   Mesh birchPlanksBlock;
-  //   Mesh acaciaPlanksBlock;
-
-  // Stone Bricks
-  //   Mesh stoneBrickBlock;
-  //   Mesh crackedStoneBricksBlock;
-  //   Mesh mossyStoneBricksBlock;
-  //   Mesh chiseledStoneBricksBlock;
-
-  // Woods
-  //   Mesh strippedOakWoodBlock;
-  //   Mesh oakLogBlock;
-  //   Mesh oakLeavesBlock;
-  /*-------------------------------*/
+  std::vector<BlockTexOffset*> blockItems;
 };
--- src/managers/block_manager.cpp
+++ src/managers/block_manager.cpp
@@ -1,5 +1,7 @@
 #include "managers/block_manager.hpp"
+#include "file/file_utils.hpp"
 
+using Tyra::FileUtils;
 using Tyra::Mesh;
 using Tyra::Renderer;
 
@@ -15,164 +17,81 @@ BlockManager::~BlockManager() {
 
 void BlockManager::init(Renderer* t_renderer) {
   this->t_renderer = t_renderer;
-  this->loadBlocks();
-  this->pushItems();
+  this->loadBlocksTextures();
+  this->registerBlocksTextureCoordinates();
 }
 
-void BlockManager::loadBlocks() {
-  // TODO: Refactor to MinecraftPipeline
-
-  //   char* MODELS_PATH = "meshes/block/";
-  //   char* TEXTURES_PATH = "assets/textures/block/";
-
-  //   // Load models:
-  //   // Blocks
-  //   stoneBlock.loadObj(MODELS_PATH, "stone", BLOCK_SIZE, true);
-  //   grassBlock.loadObj(MODELS_PATH, "grass", BLOCK_SIZE, true);
-  //   dirtBlock.loadObj(MODELS_PATH, "dirt", BLOCK_SIZE, true);
-  //   waterBlock.loadObj(MODELS_PATH, "water", BLOCK_SIZE, true);
-  //   bedrockBlock.loadObj(MODELS_PATH, "bedrock", BLOCK_SIZE, true);
-  //   sandBlock.loadObj(MODELS_PATH, "sand", BLOCK_SIZE, true);
-  //   glassBlock.loadObj(MODELS_PATH, "glass", BLOCK_SIZE, true);
-  //   bricksBlock.loadObj(MODELS_PATH, "bricks", BLOCK_SIZE, true);
-
-  //   // Ores and Minerals
-  //   goldOreBlock.loadObj(MODELS_PATH, "gold_ore", BLOCK_SIZE, true);
-  //   ironOreBlock.loadObj(MODELS_PATH, "iron_ore", BLOCK_SIZE, true);
-  //   coalOreBlock.loadObj(MODELS_PATH, "coal_ore", BLOCK_SIZE, true);
-  //   diamondOreBlock.loadObj(MODELS_PATH, "diamond_ore", BLOCK_SIZE, true);
-  //   redsoneOreBlock.loadObj(MODELS_PATH, "redstone_ore", BLOCK_SIZE, true);
-  //   emeraldOreBlock.loadObj(MODELS_PATH, "emerald_ore", BLOCK_SIZE, true);
-
-  //   // Wood Planks
-  //   oakPlanksBlock.loadObj(MODELS_PATH, "oak_planks", BLOCK_SIZE, true);
-  //   sprucePlanksBlock.loadObj(MODELS_PATH, "spruce_planks", BLOCK_SIZE,
-  //   true); birchPlanksBlock.loadObj(MODELS_PATH, "birch_planks", BLOCK_SIZE,
-  //   true); acaciaPlanksBlock.loadObj(MODELS_PATH, "acacia_planks",
-  //   BLOCK_SIZE, true);
-
-  //   // Stone Bricks
-  //   stoneBrickBlock.loadObj(MODELS_PATH, "stone_bricks", BLOCK_SIZE, true);
-  //   crackedStoneBricksBlock.loadObj(MODELS_PATH, "cracked_stone_bricks",
-  //                                   BLOCK_SIZE, true);
-  //   mossyStoneBricksBlock.loadObj(MODELS_PATH, "mossy_stone_bricks",
-  //   BLOCK_SIZE,
-  //                                 true);
-  //   chiseledStoneBricksBlock.loadObj(MODELS_PATH, "chiseled_stone_bricks",
-  //                                    BLOCK_SIZE, true);
-
-  //   // Woods
-  //   strippedOakWoodBlock.loadObj(MODELS_PATH, "stripped_oak_log", BLOCK_SIZE,
-  //                                true);
-  //   oakLogBlock.loadObj(MODELS_PATH, "oak_log", BLOCK_SIZE, true);
-  //   oakLeavesBlock.loadObj(MODELS_PATH, "oak_leaves", BLOCK_SIZE, true);
-
-  //   // Load model's Textures:
-  //   // Blocks
-  //   texRepo->addByMesh(TEXTURES_PATH, stoneBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, grassBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, dirtBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, waterBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, bedrockBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, sandBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, glassBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, bricksBlock, PNG);
-
-  //   // Ores and Minerals
-  //   texRepo->addByMesh(TEXTURES_PATH, goldOreBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, ironOreBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, coalOreBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, diamondOreBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, redsoneOreBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, emeraldOreBlock, PNG);
-
-  //   // Wood Planks
-  //   texRepo->addByMesh(TEXTURES_PATH, oakPlanksBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, sprucePlanksBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, birchPlanksBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, acaciaPlanksBlock, PNG);
-
-  //   // Stone Bricks
-  //   texRepo->addByMesh(TEXTURES_PATH, stoneBrickBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, crackedStoneBricksBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, mossyStoneBricksBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, chiseledStoneBricksBlock, PNG);
-
-  //   // Woods
-  //   texRepo->addByMesh(TEXTURES_PATH, strippedOakWoodBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, oakLogBlock, PNG);
-  //   texRepo->addByMesh(TEXTURES_PATH, oakLeavesBlock, PNG);
+void BlockManager::loadBlocksTextures() {
+  blocksTexAtlas = t_renderer->core.texture.repository.add(
+      FileUtils::fromCwd("aseets/textures/block/texture_atlas.png"));
 }
 
-void BlockManager::linkTextureByBlockType(u8 blockType, const u32 t_meshId,
-                                          u8 materialIndex) {
-  this->t_renderer->core.texture.repository
-      .getBySpriteOrMesh(
-          getMeshByBlockType(blockType)->getMaterial(materialIndex)->getId())
-      ->addLink(t_meshId);
+void BlockManager::registerBlocksTextureCoordinates() {
+  // Base Blocks
+  this->blockItems.push_back(
+      new BlockTexOffset(GRASS_BLOCK, false, 0.0F, 0.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(BEDROCK_BLOCK, true, 0.0F, 112.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(DIRTY_BLOCK, true, 16.0F, 112.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(SAND_BLOCK, true, 32.0F, 112.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(STONE_BLOCK, true, 48.0F, 112.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(WATER_BLOCK, true, 64.0F, 112.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(GLASS_BLOCK, true, 0.0F, 144.0F));
+
+  // Ores and Minerals
+  this->blockItems.push_back(
+      new BlockTexOffset(GOLD_ORE_BLOCK, true, 0.0F, 128.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(REDSTONE_ORE_BLOCK, true, 16.0F, 128.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(IRON_ORE_BLOCK, true, 32.0F, 128.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(EMERALD_ORE_BLOCK, true, 48.0F, 128.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(DIAMOND_ORE_BLOCK, true, 64.0F, 128.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(COAL_ORE_BLOCK, true, 80.0F, 128.0F));
+
+  // Stone bricks
+  this->blockItems.push_back(
+      new BlockTexOffset(STONE_BRICK_BLOCK, true, 16.0F, 96.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(CRACKED_STONE_BRICKS_BLOCK, true, 0.0F, 96.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(MOSSY_STONE_BRICKS_BLOCK, true, 32.0F, 96.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(CHISELED_STONE_BRICKS_BLOCK, true, 48.0F, 96.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(BRICKS_BLOCK, true, 64.0F, 96.0F));
+
+  // Woods
+  this->blockItems.push_back(
+      new BlockTexOffset(OAK_LOG_BLOCK, false, 10.0F, 0.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(OAK_LEAVES_BLOCK, true, 0.0F, 160.0F));
+
+  // Stripped Woods
+  this->blockItems.push_back(
+      new BlockTexOffset(STRIPPED_OAK_WOOD_BLOCK, true, 0.0F, 192.0F));
+
+  // Wood Planks
+  this->blockItems.push_back(
+      new BlockTexOffset(OAK_PLANKS_BLOCK, true, 0.0F, 176.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(SPRUCE_PLANKS_BLOCK, true, 16.0F, 176.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(ACACIA_PLANKS_BLOCK, true, 32.0F, 176.0F));
+  this->blockItems.push_back(
+      new BlockTexOffset(BIRCH_PLANKS_BLOCK, true, 48.0F, 176.0F));
 }
 
-void BlockManager::removeTextureLinkByBlockType(u8 blockType,
-                                                const u32 t_meshId,
-                                                u8 materialIndex) {
-  this->t_renderer->core.texture.repository
-      .getBySpriteOrMesh(
-          getMeshByBlockType(blockType)->getMaterial(materialIndex)->getId())
-      ->removeLinkById(t_meshId);
-}
-
-void BlockManager::pushItems() {
-  //   // Blocks
-  //   this->blockItems.push_back(new BlockItem(&grassBlock, GRASS_BLOCK));
-  //   this->blockItems.push_back(new BlockItem(&dirtBlock, DIRTY_BLOCK));
-  //   this->blockItems.push_back(new BlockItem(&stoneBlock, STONE_BLOCK));
-  //   this->blockItems.push_back(new BlockItem(&waterBlock, WATER_BLOCK));
-  //   this->blockItems.push_back(new BlockItem(&bedrockBlock, BEDROCK_BLOCK));
-  //   this->blockItems.push_back(new BlockItem(&sandBlock, SAND_BLOCK));
-  //   this->blockItems.push_back(new BlockItem(&glassBlock, GLASS_BLOCK));
-  //   this->blockItems.push_back(new BlockItem(&bricksBlock, BRICKS_BLOCK));
-
-  //   // Ores and Minerals
-  //   this->blockItems.push_back(new BlockItem(&goldOreBlock, GOLD_ORE_BLOCK));
-  //   this->blockItems.push_back(new BlockItem(&ironOreBlock, IRON_ORE_BLOCK));
-  //   this->blockItems.push_back(new BlockItem(&coalOreBlock, COAL_ORE_BLOCK));
-  //   this->blockItems.push_back(
-  //       new BlockItem(&diamondOreBlock, DIAMOND_ORE_BLOCK));
-  //   this->blockItems.push_back(
-  //       new BlockItem(&redsoneOreBlock, REDSTONE_ORE_BLOCK));
-  //   this->blockItems.push_back(
-  //       new BlockItem(&emeraldOreBlock, EMERALD_ORE_BLOCK));
-
-  //   // Wood Planks
-  //   this->blockItems.push_back(new BlockItem(&oakPlanksBlock,
-  //   OAK_PLANKS_BLOCK)); this->blockItems.push_back(
-  //       new BlockItem(&sprucePlanksBlock, SPRUCE_PLANKS_BLOCK));
-  //   this->blockItems.push_back(
-  //       new BlockItem(&birchPlanksBlock, BIRCH_PLANKS_BLOCK));
-  //   this->blockItems.push_back(
-  //       new BlockItem(&acaciaPlanksBlock, ACACIA_PLANKS_BLOCK));
-
-  //   // Stone bricks
-  //   this->blockItems.push_back(
-  //       new BlockItem(&stoneBrickBlock, STONE_BRICK_BLOCK));
-  //   this->blockItems.push_back(
-  //       new BlockItem(&crackedStoneBricksBlock, CRACKED_STONE_BRICKS_BLOCK));
-  //   this->blockItems.push_back(
-  //       new BlockItem(&mossyStoneBricksBlock, MOSSY_STONE_BRICKS_BLOCK));
-  //   this->blockItems.push_back(
-  //       new BlockItem(&chiseledStoneBricksBlock,
-  //       CHISELED_STONE_BRICKS_BLOCK));
-
-  //   // Woods
-  //   this->blockItems.push_back(
-  //       new BlockItem(&strippedOakWoodBlock, STRIPPED_OAK_WOOD_BLOCK));
-  //   this->blockItems.push_back(new BlockItem(&oakLogBlock, OAK_LOG_BLOCK));
-  //   this->blockItems.push_back(new BlockItem(&oakLeavesBlock,
-  //   OAK_LEAVES_BLOCK));
-}
-
-Mesh* BlockManager::getMeshByBlockType(u8 blockType) {
+BlockTexOffset* BlockManager::getBlockTexOffsetByType(u8 blockType) {
   for (int i = 0; i < blockItems.size(); i++)
-    if (blockItems[i]->blockId == blockType) return blockItems[i]->t_mesh;
+    if (blockItems[i]->blockId == blockType) return blockItems[i];
   return NULL;
 }
