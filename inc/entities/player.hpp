3290599b82a992fb20f8d28002e234023995aaea
Wellinator <wellcoj@gmail.com>
2022-10-04 16:22:59 -0300

    fix: player jump height

--- inc/entities/player.hpp
+++ inc/entities/player.hpp
@@ -84,7 +84,7 @@ class Player {
   float speed = 100;
 
   // Phisycs values
-  Vec4 lift = Vec4(0.0f, -5.0F, 0.0f);
+  Vec4 lift = Vec4(0.0f, -2.8F, 0.0f);
   Vec4 velocity = Vec4(0.0f, 0.0f, 0.0f);
   BBox* hitBox;
 
