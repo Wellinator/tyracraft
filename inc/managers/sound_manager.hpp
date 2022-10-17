38bbff1ecd644b74f864478347993091ebac9acc
Wellinator <wellcoj@gmail.com>
2022-10-17 15:55:26 -0300

    feat: implements getter methods

--- inc/managers/sound_manager.hpp
+++ inc/managers/sound_manager.hpp
@@ -10,6 +10,17 @@ class SoundManager {
   SoundManager(Engine* t_engine);
   ~SoundManager();
 
+  SfxLibraryCategory* getCategory(SoundFxCategory idCategory) {
+    this->soundLibrary->getCategory(idCategory);
+  }
+
+  SfxLibrarySound* getSound(SoundFxCategory idCategory, SoundFX idSound) {
+    this->soundLibrary->getSound(idCategory, idSound);
+  }
+
+  void playSfx(SoundFxCategory idCategory, SoundFX idSound);
+
  private:
   SfxLibrary* soundLibrary;
+  Engine* t_engine;
 };
