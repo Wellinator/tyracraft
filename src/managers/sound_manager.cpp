ae808fdb5620de4e779e58d9cd122a9a5ac31ee4
Wellinator <wellcoj@gmail.com>
2022-10-17 15:55:45 -0300

    feat: implements playSfx

--- src/managers/sound_manager.cpp
+++ src/managers/sound_manager.cpp
@@ -1,7 +1,14 @@
 #include "managers/sound_manager.hpp"
 
 SoundManager::SoundManager(Engine* t_engine) {
+  this->t_engine = t_engine;
   this->soundLibrary = new SfxLibrary(&t_engine->audio);
 }
 
 SoundManager::~SoundManager() { delete soundLibrary; }
+
+void SoundManager::playSfx(SoundFxCategory idCategory, SoundFX idSound) {
+  SfxLibrarySound* t_sound = this->getSound(idCategory, idSound);
+  this->t_engine->audio.adpcm.tryPlay(t_sound->_sound);
+  this->t_engine->audio.adpcm.playWait(t_sound->_sound);
+};
