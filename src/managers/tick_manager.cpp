7d35dea26d4e4c3f461746966b5be407f46460e2
Wellinator <wellcoj@gmail.com>
2022-12-09 10:50:18 -0300

    feat: creates global tick manager

---
+++ src/managers/tick_manager.cpp
@@ -0,0 +1,27 @@
+#include "managers/tick_manager.hpp"
+
+/**
+ * @brief Definition of global tick variables
+ *
+ */
+double elapsedRealTime = 0;
+float ticksCounter = DAY_MID;
+u16 ticksDayCounter = 0;
+
+TickManager::TickManager() {}
+
+TickManager::~TickManager() {}
+
+void TickManager::update(const float& deltaTime) {
+  updateTicks(std::min(deltaTime, MAX_FRAME_MS));
+}
+
+void TickManager::updateTicks(const float& deltaTime) {
+  elapsedRealTime += deltaTime;
+  ticksCounter += deltaTime / TICK;
+
+  if (ticksCounter >= DAY_DURATION_IN_TICKS) {
+    ticksDayCounter++;
+    ticksCounter -= DAY_DURATION_IN_TICKS;
+  }
+}
