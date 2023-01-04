59770ce666e8294f1a504abf67e78fd69e5e1dd5
Estem0 <101932306+Estem01@users.noreply.github.com>
2023-01-04 20:27:05 -0300

    Arrumei

--- inc/managers/itemid_repository.hpp
+++ inc/managers/itemid_repository.hpp
@@ -19,35 +19,35 @@ class ItemIdRepository {
   // Item Id
 
   // Blocks
-  Item dirt 1;
-  Item sand 3;
-  Item stone 2;
-  Item bricks 4;
-  Item glass 5;
+  Item dirt = 1;
+  Item sand =  3;
+  Item stone = 2;
+  Item bricks = 4;
+  Item glass = 5;
 
   // Ores and Minerals blocks
-  Item coal_ore_block 6;
-  Item diamond_ore_block 9;
-  Item iron_ore_block 7;
-  Item gold_ore_block 8;
-  Item redstone_ore_block 10;
-  Item emerald_ore_block 11;
+  Item coal_ore_block = 6;
+  Item diamond_ore_block = 9;
+  Item iron_ore_block = 7;
+  Item gold_ore_block = 8;
+  Item redstone_ore_block = 10;
+  Item emerald_ore_block = 11;
 
   // Wood Planks
-  Item oak_planks 12;
-  Item spruce_planks 13;
-  Item birch_planks 14;
-  Item acacia_planks 15;
+  Item oak_planks = 12;
+  Item spruce_planks = 13;
+  Item birch_planks = 14;
+  Item acacia_planks = 15;
 
   // Stone Bricks
-  Item stone_brick 16;
-  Item cracked_stone_bricks 17;
-  Item mossy_stone_bricks 18;
-  Item chiseled_stone_bricks 19;
+  Item stone_brick = 16;
+  Item cracked_stone_bricks = 17;
+  Item mossy_stone_bricks  =18;
+  Item chiseled_stone_bricks = 19;
 
   // Stripped woods
-  Item stripped_oak_wood 20;
+  Item stripped_oak_wood = 20;
 
   // Toolds
-  Item wooden_axe 21;
+  Item wooden_axe = 21;
 };
