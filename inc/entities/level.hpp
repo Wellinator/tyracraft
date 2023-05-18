149f19fb8bb7f9db5edff2efb6593541f118125a
Wellinator <wellcoj@gmail.com>
2023-05-18 08:18:07 -0300

    fix: light data access get and set

--- inc/entities/level.hpp
+++ inc/entities/level.hpp
@@ -11,10 +11,12 @@ typedef struct {
   uint16_t spawnX, spawnY, spawnZ;
 
   uint8_t* blocks;
+  uint8_t* lightData;
 
   // block data << 4
   // light data & 0x0F
   uint8_t* data;
+
 } LevelMap;
 
 typedef struct {
@@ -27,9 +29,15 @@ void GetXYZFromPos(uint32_t pos, uint32_t* x, uint32_t* y, uint32_t* z);
 
 uint8_t GetDataFromMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z);
 uint8_t GetLightFromMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z);
+uint8_t GetBlockLightFromMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z);
+uint8_t GetSunLightFromMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z);
 uint8_t GetBlockFromMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z);
 
-void SetBlockInMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z, uint8_t block);
-void SetLightInMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z, uint16_t light);
+void SetBlockInMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z,
+                   uint8_t block);
+void SetBlockLightInMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z,
+                        uint16_t light);
+void SetSunLightInMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z,
+                      uint16_t light);
 
 bool BoundCheckMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z);
--- src/entities/level.cpp
+++ src/entities/level.cpp
@@ -2,47 +2,69 @@
 
 // Gets the position in the data array from the given x, y, and z coordinates.
 uint32_t GetPosFromXYZ(uint32_t x, uint32_t y, uint32_t z) {
-    return x + (y << 10) + (z << 20);
+  return x + (y << 10) + (z << 20);
 }
 
 // Gets the x, y, and z coordinates from the given position in the data array.
 void GetXYZFromPos(uint32_t pos, uint32_t* x, uint32_t* y, uint32_t* z) {
-    *x = pos % 1024;
-    *y = (pos >> 10) % 1024;
-    *z = (pos >> 20) % 1024;
+  *x = pos % 1024;
+  *y = (pos >> 10) % 1024;
+  *z = (pos >> 20) % 1024;
 }
 
 // Gets the data value at the given coordinates in the map.
 uint8_t GetDataFromMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z) {
-    uint32_t index = (y * map->length * map->width) + (z * map->width) + x;
-    return map->data[index];
+  uint32_t index = (y * map->length * map->width) + (z * map->width) + x;
+  return map->data[index];
 }
 
 // Gets the light value at the given coordinates in the map.
 uint8_t GetLightFromMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z) {
-    uint8_t data = GetDataFromMap(map, x, y, z);
-    return data & 0x0F;
+  uint32_t index = (y * map->length * map->width) + (z * map->width) + x;
+  uint8_t v = map->lightData[index];
+  uint8_t res = ((v & 0xF0) >> 4) + (v & 0x0F);
+
+  if (res > 0x0F) return 0x0F;
+  return res;
 }
 
 // Gets the block ID at the given coordinates in the map.
 uint8_t GetBlockFromMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z) {
-    uint32_t index = (y * map->length * map->width) + (z * map->width) + x;
-    return map->blocks[index];
+  uint32_t index = (y * map->length * map->width) + (z * map->width) + x;
+  return map->blocks[index];
 }
 
 // Sets the block ID at the given coordinates in the map.
-void SetBlockInMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z, uint8_t block) {
-    uint32_t index = (y * map->length * map->width) + (z * map->width) + x;
-    map->blocks[index] = block;
+void SetBlockInMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z,
+                   uint8_t block) {
+  uint32_t index = (y * map->length * map->width) + (z * map->width) + x;
+  map->blocks[index] = block;
 }
 
-// Sets the light value at the given coordinates in the map.
-void SetLightInMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z, uint16_t light) {
-    uint32_t index = (y * map->length * map->width) + (z * map->width) + x;
-    map->data[index] = (map->data[index] & 0xF0) | (light & 0xF);
+uint8_t GetBlockLightFromMap(LevelMap* map, uint16_t x, uint16_t y,
+                             uint16_t z) {
+  uint32_t index = (y * map->length * map->width) + (z * map->width) + x;
+  return map->lightData[index] & 0x0F;
+}
+
+uint8_t GetSunLightFromMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z) {
+  uint32_t index = (y * map->length * map->width) + (z * map->width) + x;
+  return (map->lightData[index] & 0xF0);
+}
+
+void SetBlockLightInMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z,
+                        uint16_t light) {
+  uint32_t index = (y * map->length * map->width) + (z * map->width) + x;
+  map->lightData[index] = (map->lightData[index] & 0xF0) | light;
+}
+
+void SetSunLightInMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z,
+                      uint16_t light) {
+  uint32_t index = (y * map->length * map->width) + (z * map->width) + x;
+  map->lightData[index] = (map->lightData[index] & 0x0F) | (light << 4);
 }
 
 // Returns true if the given coordinates are within the bounds of the map.
 bool BoundCheckMap(LevelMap* map, uint16_t x, uint16_t y, uint16_t z) {
-    return (x < map->length && y < map->height && z < map->width);
+  return (x < map->length && y < map->height && z < map->width);
 }
