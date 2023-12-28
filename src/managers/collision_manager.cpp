1d6127337bf0a0bc5613414aa9f364526af39fea
Wellinator <wellcoj@gmail.com>
2023-12-28 10:54:14 -0300

    fix: clear aabb tree after unloading world

--- src/managers/collision_manager.cpp
+++ src/managers/collision_manager.cpp
@@ -9,6 +9,7 @@ AABBTree* g_AABBTree = nullptr;
 void CollisionManager_initTree() { g_AABBTree = new AABBTree(); };
 
 void CollisionManager_unloadTree() {
+  g_AABBTree->clear();
   delete g_AABBTree;
   g_AABBTree = nullptr;
 };
