3ed557b4b93a28e9871cfc02abc2201803fd3603
Wellinator <wellcoj@gmail.com>
2024-06-11 09:20:33 -0300

    chore: added GameMode prop to NewGameOptions model

--- inc/models/new_game_model.hpp
+++ inc/models/new_game_model.hpp
@@ -10,6 +10,7 @@ class NewGameOptions {
   uint32_t seed = 0;
   u8 drawDistance = MIN_DRAW_DISTANCE;
   WorldType type = WorldType::WORLD_TYPE_ORIGINAL;
+  GameMode gameMode = GameMode::Creative;
   float initialTime = 1000;  // Sun rise in ticks
   std::string texturePack = "default";
   std::string name;
