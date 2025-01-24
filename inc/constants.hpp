185217ea179be083618f07b0d3be01274701b98a
Wellinator <wellcoj@gmail.com>
2025-01-24 08:57:24 -0300

    chore: reduce chunk loading speed

--- inc/constants.hpp
+++ inc/constants.hpp
@@ -56,8 +56,8 @@
 
 // Define how many blocks will be loaded/unloaded from chunk per step in async
 // loading
-#define UNLOAD_CHUNK_BATCH 128
-#define LOAD_CHUNK_BATCH 128
+#define UNLOAD_CHUNK_BATCH 48
+#define LOAD_CHUNK_BATCH 48
 
 // Texture atlas info
 #define MAX_TEX_COLS 16
