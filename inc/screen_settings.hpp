84375f13cc2f346c7ace360ad5d07239f6d1a1fe
Wellinator <wellcoj@gmail.com>
2025-12-03 10:42:42 -0300

    feat: add screen settings configuration for rendering

---
+++ inc/screen_settings.hpp
@@ -0,0 +1,7 @@
+/*---------------------------------------------*/
+// Screen settings
+#define SCREEN_WIDTH 512.0F
+#define SCREEN_HEIGHT 448.0F
+#define SCREEN_NEAR_PLANE 0.01F
+#define SCREEN_FAR_PLANE 5000.0F
+/*---------------------------------------------*/
